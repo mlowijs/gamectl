c63fffd5c83feaeb57ecc3b94f604e094f1ce13d
Michiel Lowijs <mlowijs@gmail.com>
2023-12-21 12:42:29 +0100

    small fix

--- src/Gamectl/Gamescope.cs
+++ src/Gamectl/Gamescope.cs
@@ -4,12 +4,9 @@ public static class Gamescope
 {
     public static string GetGamescopeCommandLine(int fps, string command)
     {
-        if (Configuration.Values.GamescopeArguments is null)
-            return command;
-
         if (Drm.GetCards().Length > 1 && !Configuration.Values.GamescopeOnExternalGpu)
             return command;
         
-        return $"gamescope {Configuration.Values.GamescopeArguments} -r {fps} -- {command}";
+        return $"gamescope {Configuration.Values.GamescopeArguments ?? ""} -r {fps} -- {command}";
     }
 }
